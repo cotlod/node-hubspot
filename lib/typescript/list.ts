abef30022c22b346509415abee19d698b12f6dec
Mathieu Cote <mathieu.cote@ogma.app>
2019-08-13 14:55:08 -0400

    changed types to optionals

--- lib/typescript/list.ts
+++ lib/typescript/list.ts
@@ -1,12 +1,12 @@
 import { RequestPromise } from 'request-promise'
 
 class GetContactsOption {
-  count: number = 20
-  vidOffset: number
-  propertyMode: 'value_only' | 'value_and_history' = 'value_only'
-  formSubmissionMode: 'all' | 'none' | 'newest' | 'oldest' = 'newest'
-  showListMemberships: boolean = false
-  property: string
+  count?: number = 20
+  vidOffset?: number
+  propertyMode?: 'value_only' | 'value_and_history' = 'value_only'
+  formSubmissionMode?: 'all' | 'none' | 'newest' | 'oldest' = 'newest'
+  showListMemberships?: boolean = false
+  property?: string
 }
 
 declare class List {
