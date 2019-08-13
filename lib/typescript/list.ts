e5dafb3e4ac9e5c765ff8881626c8402dfabc900
Mathieu Cote <mathieu.cote@ogma.app>
2019-08-13 14:35:02 -0400

    added missing opts from getContacts and type

--- lib/typescript/list.ts
+++ lib/typescript/list.ts
@@ -1,15 +1,24 @@
 import { RequestPromise } from 'request-promise'
 
+class GetContactsOption {
+  count: number = 20
+  vidOffset: number
+  propertyMode: 'value_only' | 'value_and_history' = 'value_only'
+  formSubmissionMode: 'all' | 'none' | 'newest' | 'oldest' = 'newest'
+  showListMemberships: boolean = false
+  property: string
+}
+
 declare class List {
   get(opts?: {}): RequestPromise
 
   getOne(id: number): RequestPromise
 
-  getContacts(id: number): RequestPromise
+  getContacts(id: number, opts?: GetContactsOption): RequestPromise
 
   getRecentContacts(id: number): RequestPromise
 
   addContacts(id: number, contactBody: {}): RequestPromise
 }
 
-export { List }
+export { List, GetContactsOption }
